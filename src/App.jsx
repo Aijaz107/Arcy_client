9d5a598653f9319f890a17f80bd2f891fab8ef8e
Aijaz107 <54859901+Aijaz107@users.noreply.github.com>
2021-08-15 13:02:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,17 +11,17 @@ const App = () => {
     <Router>
       <Switch>
       <Route exact path="/" component={Main} />
-      <Route exact path="/g1" component={SubGal} />
+      <Route  path="/g1" component={SubGal} />
       <Route exact path="/interior_gallery" component={InteriorGallery} />
       <Route exact path="/upvc_gallery" component={UpvcGallery} />
-//       <Route exact path="/g2" component={SubGal} />
-//       <Route exact path="/g3" component={SubGal} />
-//       <Route exact path="/g4" component={SubGal} />
-//       <Route exact path="/g5" component={SubGal} />
-//       <Route exact path="/g6" component={SubGal} />
-//       <Route exact path="/g7" component={SubGal} />
-//       <Route exact path="/g8" component={SubGal} />
-//       <Route exact path="/g9" component={SubGal} />
+      <Route  path="/g2" component={SubGal} />
+      <Route exact path="/g3" component={SubGal} />
+      <Route exact path="/g4" component={SubGal} />
+      <Route exact path="/g5" component={SubGal} />
+      <Route exact path="/g6" component={SubGal} />
+      <Route exact path="/g7" component={SubGal} />
+      <Route exact path="/g8" component={SubGal} />
+      <Route exact path="/g9" component={SubGal} />
 
       </Switch>
     </Router>
