245797483196c2768c74a6aae78d80a243d1da7a
Aijaz107 <54859901+Aijaz107@users.noreply.github.com>
2022-02-20 16:03:19 +0530

    Update services.jsx

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -5,8 +5,7 @@ export const Services = (props) => {
         <div className='section-title'>
           <h2>Our Services</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+          
           </p>
         </div>
         <div className='row'>
